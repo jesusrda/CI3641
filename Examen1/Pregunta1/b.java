49073ccc57949e936a30ee5426e432ee6f62eef8
jesusrda <jrdadb@gmail.com>
2021-02-17 21:36:17 -0400

    Añadiendo ejercicio b, pregunta 1

---
+++ Examen1/Pregunta1/b.java
@@ -0,0 +1,30 @@
+class Ejercicio1b {
+
+    public static int Fact(int n) {
+        
+        if (n == 0) 
+            return 1;
+
+        return n*Fact(n-1);
+    }
+
+    public static int[][] Prod(int[][] A,
+                               int[][] B,
+                               int N,
+                               int M,
+                               int P){
+        
+        int[][] C = new int[N][P];
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < P; j++){ 
+                C[i][j] = 0;
+                for(int k=0; k < M; k++){
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        return C;                       
+    }
+}
